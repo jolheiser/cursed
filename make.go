ebc0090d711c8a95464c19e5dd14b32a6a8bfc5d
ShadiestGoat <48590492+ShadiestGoat@users.noreply.github.com>
2022-07-28 01:08:45 +0100

    Call me an exorcist because I just cured a curse

--- make.go
+++
@@ -1,6 +0,0 @@
-package cursed
-
-// Make makes a []T
-func Make[T any]() []T {
-	return make([]T, len([]T{}))
-}
--- ternary.go
+++
@@ -1,9 +0,0 @@
-package cursed
-
-// ᕈ is the ternary operator...sort of
-func ᕈ[T any](expr bool, t, f T) T {
-	if expr {
-		return t
-	}
-	return f
-}
