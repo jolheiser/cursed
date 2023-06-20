bd537e600a09c141200800b95ae003a8745160ff
jolheiser <john.olheiser@gmail.com>
2023-06-20 15:06:44 -0500

    feat: use ternary for ptr
    Signed-off-by: jolheiser <john.olheiser@gmail.com>

--- ptr.go
+++ ptr.go
@@ -2,5 +2,5 @@ package cursed
 
 // NewPtr returns a new pointer to T, or an empty T if no args are provided
 func NewPtr[T any](in ...T) *T {
-	return map[bool]func() *T{true: func() *T { return &(&struct{ v T }{in[0]}).v }, false: func() *T { return &(&struct{ v T }{}).v }}[len(in) > 0]()
+	return ᕈ(len(in) > 0, func() *T { return &(&struct{ v T }{in[0]}).v }, func() *T { return &(&struct{ v T }{}).v })()
 }
